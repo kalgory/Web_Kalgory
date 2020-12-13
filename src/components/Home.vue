e2975e18aed2b5b87041adff37d769cef10ee3e5
Hyunmin Jung <jung@hyunmin.dev>
2020-12-13 20:34:08 +0900

    First Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>
+     {{ name }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      name: 'JHM',
+
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
