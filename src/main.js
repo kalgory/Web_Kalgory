1eff99c87e9cbd0ddf08a779d649e7cc7110a5e3
Hyunmin Jung <jung@hyunmin.dev>
2021-01-11 00:31:48 +0900

    chore: add firebase setting file

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import './plugins/firebase'
+
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
-
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
