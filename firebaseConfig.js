e92155fa507f7432f435bda19afe85c30f01a304
Hyunmin Jung <jung@hyunmin.dev>
2021-01-11 20:11:09 +0900

    refactor: Erase not use comment & shorten export

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export default {
   apiKey: 'AIzaSyAJOEgnBYIoBRJmvGYqi4WUC1l3ZORLuAM',
   authDomain: 'kalgory-dev.firebaseapp.com',
   projectId: 'kalgory-dev',
@@ -8,5 +8,3 @@ const firebaseConfig = {
   appId: '1:271562870658:web:96a9f0e808e9dc6100499b',
   measurementId: 'G-CN7J15NRCE',
 }
-
-export default firebaseConfig
