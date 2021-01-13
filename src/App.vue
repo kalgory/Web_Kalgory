dd42ec9d986e286ce9bb3bbe744f7bcd0e2d708d
Hyunmin Jung <jung@hyunmin.dev>
2021-01-13 20:30:29 +0900

    feat: redesign App.vue component structure

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,21 @@
 <template>
   <v-app>
     <v-app-bar
-        app
-        color="white"
-        flat
+      app
+      color="#121212"
     >
-      <v-container class="py-0 fill-height">
-        <v-avatar
-            class="mr-10"
-            color="grey darken-1"
-            size="32"
-        ></v-avatar>
 
-        <v-btn
-            v-for="link in links"
-            :key="link"
-            text
-        >
-          {{ link }}
-        </v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-responsive max-width="260">
-          <v-text-field
-              dense
-              flat
-              hide-details
-              rounded
-              solo-inverted
-          ></v-text-field>
-        </v-responsive>
-
-        <v-btn
-            text
-        >
-          Sign in
-        </v-btn>
-        <v-btn
-            outlined
-            text
-        >
-          Sign up
-        </v-btn>
-      </v-container>
     </v-app-bar>
 
-    <v-main class="grey lighten-3">
-      <v-container>
-        <v-row>
-          <v-col cols="2">
-            <v-sheet rounded="lg">
-              <v-list color="transparent">
-                <v-list-item
-                    v-for="n in 5"
-                    :key="n"
-                    link
-                >
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      List Item {{ n }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
+    <v-main>
 
-                <v-divider class="my-2"></v-divider>
-
-                <v-list-item
-                    link
-                    color="grey lighten-4"
-                >
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      Refresh
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-sheet>
-          </v-col>
-
-          <v-col>
-            <v-sheet
-                min-height="70vh"
-                rounded="lg"
-            >
-
-              <!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
     </v-main>
+    
+    <v-footer
+      app
+    >
+      <p class="ma-0 font-weight-thin">Copyright © 2021 Kalgory</p>
+    </v-footer>
   </v-app>
 </template>
 
