85a31934b9977cf3e771021f13dee3f0e0725b1d
Hyunmin Jung <jung@hyunmin.dev>
2021-01-15 23:45:33 +0900

    feat: modify frame of app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,31 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="#121212"
-    >
-      <v-img
-        src="./assets/logo-initial-background.svg"
-      >
-      </v-img>
-      <v-img
-        src="./assets/logo-text.svg"
-      >
-      </v-img>
-
+    <v-app-bar app color="">
+      <initial-logo></initial-logo>
+      <v-spacer/>
+      <v-btn>qwerty</v-btn>
     </v-app-bar>
-
+    
     <v-main>
       <router-view></router-view>
     </v-main>
     
-    <v-footer
-      app
-    >
+    <v-footer app>
       <p class="ma-0 font-weight-thin">Copyright © 2021 Kalgory</p>
     </v-footer>
   </v-app>
 </template>
 
-
 <script>
-
+import InitialLogo from '@/assets/InitialLogo'
 
 export default {
   name: 'App',
-
-  components: {},
-
+  
+  components: {
+    InitialLogo,
+  },
+  
   data: () => ({
     links: [
       'test',
