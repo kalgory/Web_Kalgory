56bfaf94bbe554b77d14472dfdfd13bcc79a35d9
Dongmin Kim <ehdals0211@naver.com>
2021-01-23 22:31:09 +0900

    community question container create

---
+++ src/components/community/question/QuestionForm.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "QuestionForm"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/community/BaseBoard.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "BaseBoard"
+}
+</script>
+
+<style scoped>
+
+</style>
