87ab480c4c180f165dea9b84c0ca508dbe0999c5
Hyunmin Jung <jung@hyunmin.dev>
2021-01-26 00:04:51 +0900

    style: refactor routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -21,7 +21,7 @@ const QuestionThread = () => import('@/views/community/question/QuestionThread')
 
 const requireUnauthorized = () => (to, from, next) => {
   if (Store.getters.getIsLoading) {
-    if (localStorage.getItem('isAuth') === "true") {
+    if (localStorage.getItem('isAuth') === 'true') {
       next('/')
     } else {
       next()
@@ -30,84 +30,81 @@ const requireUnauthorized = () => (to, from, next) => {
     if (Store.getters.getIsAuth) {
       next('/')
     } else {
-        if (Store.getters.getIsAuth) {
-            next('/')
-        } else {
-            next()
-        }
+      next()
     }
+  }
 }
 
 export default [
-    {
-        name: 'home',
-        path: '/',
-        redirect: '/concept',
-    },
-    {
-        path: '*',
-        component: Base404,
-    },
-    {
-        name: 'problem',
-        path: '/problem',
-        component: BaseProblem,
-    },
-    {
-        name: 'concept',
-        path: '/concept',
-        component: BaseConcept,
-    },
-    {
-        name: 'auth',
-        path: '/auth',
-        component: BaseAuth,
-        redirect: '/auth/signin',
-        beforeEnter: requireUnauthorized(),
+  {
+    name: 'home',
+    path: '/',
+    redirect: '/concept',
+  },
+  {
+    path: '*',
+    component: Base404,
+  },
+  {
+    name: 'problem',
+    path: '/problem',
+    component: BaseProblem,
+  },
+  {
+    name: 'concept',
+    path: '/concept',
+    component: BaseConcept,
+  },
+  {
+    name: 'auth',
+    path: '/auth',
+    component: BaseAuth,
+    redirect: '/auth/signin',
+    beforeEnter: requireUnauthorized(),
+    children: [
+      {
+        name: 'sign in',
+        path: 'signin',
+        component: AuthSignIn,
+      },
+      {
+        name: 'sign up',
+        path: 'signup',
+        component: AuthSignUp,
+      },
+    ],
+  },
+  {
+    name: 'community',
+    path: '/community',
+    redirect: '/community',
+    component: BaseCommunity,
+    children: [
+      {
+        name: 'base board',
+        path: '',
+        component: CommunityDefault,
+      },
+      {
+        name: 'question',
+        path: 'question',
+        redirect: { name: 'question thread' },
+        component: CommunityQuestion,
         children: [
-            {
-                name: 'sign in',
-                path: 'signin',
-                component: AuthSignIn,
-            },
-            {
-                name: 'sign up',
-                path: 'signup',
-                component: AuthSignUp,
-            },
+          {
+            name: 'question thread',
+            path: ':id',
+            props: { id: true, currentThread: true },
+            component: QuestionThread,
+          },
+        
         ],
-    },
-    {
-        name: 'community',
-        path: '/community',
-        redirect: '/community',
-        component: BaseCommunity,
-        children: [
-            {
-                name: 'base board',
-                path: '',
-                component: CommunityDefault,
-            },
-            {
-                name: 'question',
-                path: 'question',
-                redirect: {name: 'question thread'},
-                component: CommunityQuestion,
-                children: [
-                    {
-                        name: 'question thread',
-                        path: ':id',
-                        props: {id: true, currentThread: true},
-                        component: QuestionThread
-                    },
-
-                ]
-            },
-            {
-                name: 'information',
-                path: 'information',
-                component: CommunityInformation,
-            },
-        ],
-    },
+      },
+      {
+        name: 'information',
+        path: 'information',
+        component: CommunityInformation,
+      },
+    ],
+  },
 ]
