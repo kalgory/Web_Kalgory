6807f543ed753bd1de0f12116e4a5b74d94a9514
Dongmin Kim <ehdals0211@naver.com>
2021-01-25 22:31:43 +0900

    feat: community card action update

--- src/components/community/CommunityCardAction.vue
+++ src/components/community/CommunityCardAction.vue
@@ -2,11 +2,11 @@
   <v-card-actions>
     <v-row justify="center" align="end">
       <v-col cols="5">
-        <v-btn block height="100" outlined class="text-h5 font-weight-bold"
+        <v-btn block outlined class="font-weight-bold"
                @click="$router.push({name:'question'})">Question And Answer</v-btn>
       </v-col>
       <v-col cols="5">
-        <v-btn block height="100" outlined class="text-h5 font-weight-bold"
+        <v-btn block outlined class="font-weight-bold"
                @click="$router.push({name:'information'})">Information</v-btn>
       </v-col>
     </v-row>
