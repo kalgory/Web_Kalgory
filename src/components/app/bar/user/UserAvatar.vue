31ba85b21e2a0502e2dc306bab60135a6c49f805
Hyunmin Jung <jung@hyunmin.dev>
2021-01-29 21:22:05 +0900

    feat: add bar user with user avatar

---
+++ src/components/app/bar/user/UserAvatar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-avatar
+    color="primary"
+    :size="size"
+  >
+    <v-img
+      v-if="isPhotoExist"
+      lazy-src="photo"
+      src="photo"
+    />
+    <span
+      v-if="!isPhotoExist"
+      class="white--text headline"
+    >{{ name }}</span>
+  </v-avatar>
+</template>
+
+<script>
+export default {
+  name: 'UserAvatar',
+  props: {
+    size: {
+      type: Number,
+      default: 48,
+      required: false,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    isPhotoExist() {
+      return !!this.photo;
+    },
+  },
+
+};
+</script>
