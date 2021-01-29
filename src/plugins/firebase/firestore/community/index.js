bf6f57bb2112991f1b5ec6e17198a2a0d64831f2
Dongmin Kim <ehdals0211@naver.com>
2021-01-29 14:35:23 +0900

    feat: community firesotre function update

--- src/plugins/firebase/firestore/community/index.js
+++ src/plugins/firebase/firestore/community/index.js
@@ -1,37 +1,31 @@
 // import Firebase from 'firebase/app';
-import { getQuestionReference, getInformationReference } from './reference';
 
-function changeDateFormat(timestamp) {
-  const date = timestamp.toDate();
-  // const now = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const day = date.getDate();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
-  // if (now.getDate() === day) {
-  //   return `${hour}시 ${minute}분`;
-  // } if (now.getMonth() === month) {
-  //   return `${day}일 ${hour}시 ${minute}분`;
-  // } if (now.getFullYear() === year) {
-  //   return `${month}월 ${day}일 ${hour}시 ${minute}분`;
-  // }
-  return `${year}년${month}월 ${day}일 ${hour}시 ${minute}분`;
+export function readPosts(reference, count = null) {
+  return new Promise((resolve, reject) => {
+    reference.orderBy('created_time', 'desc').limit(count).get()
+      .then((snapshot) => {
+        const posts = [];
+        snapshot.forEach((document) => {
+          const newPost = document.data();
+          newPost.id = document.id;
+          posts.push(newPost);
+        });
+        resolve(posts);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
-export default function readNewPost(threadType, threadList) {
-  let ref;
-  if (threadType === 'questions')ref = getQuestionReference();
-  else if (threadType === 'information')ref = getInformationReference();
-  ref.orderBy('created_time', 'desc').limit(5).get().then((sn) => {
-    sn.forEach((doc) => {
-      const newThread = doc.data();
-      newThread.id = doc.id;
-      newThread.created_time = changeDateFormat(doc.data().created_time);
-      threadList.push(newThread);
-    });
-  })
-    .catch((err) => {
-      console.error(err);
-    });
+export function readPost(reference, documentID) {
+  return new Promise((resolve, reject) => {
+    reference.doc(documentID).get()
+      .then((snapshot) => {
+        resolve(snapshot.data());
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
