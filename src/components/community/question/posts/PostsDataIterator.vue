0f833737be6f39e78626af8acfc5639b9e3e163e
Dongmin Kim <ehdals0211@naver.com>
2021-02-01 16:09:28 +0900

    feat: Change component simple to dataitreator

--- src/components/community/question/posts/PostsDataIterator.vue
+++ src/components/community/question/posts/PostsDataIterator.vue
@@ -1,10 +1,65 @@
 <template>
-  <v-data-iterator />
+  <v-data-iterator
+    :items="questionPosts"
+    :search="searchPost"
+    hide-default-footer
+  >
+    <template #header>
+      <v-row justify="center">
+        <v-col
+          cols="8"
+        >
+          <v-text-field
+            v-model="searchPost"
+            prepend-inner-icon="mdi-table-search"
+          />
+        </v-col>
+      </v-row>
+    </template>
+    <template #default="props">
+      <v-row
+        v-for="post in props.items"
+        :key="post.body"
+        justify="center"
+      >
+        <v-col
+          cols="8"
+        >
+          <v-card>
+            <v-card-title class="font-weight-bold">
+              {{ post.header }}
+            </v-card-title>
+            <v-card-text>
+              {{ post.body }}
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </v-data-iterator>
 </template>
 
 <script>
+import { readPosts } from '@/plugins/firebase/firestore/community';
+import { getQuestionCommunityReference } from '@/plugins/firebase/firestore/community/reference';
+
 export default {
   name: 'QuestionDataIterator',
+
+  data: () => ({
+    questionPosts: [],
+    searchPost: '',
+  }),
+
+  created() {
+    readPosts(getQuestionCommunityReference(), 10)
+      .then((posts) => {
+        this.questionPosts = posts;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
 };
 </script>
 
--- src/views/community/question/QuestionPosts.vue
+++ src/views/community/question/QuestionPosts.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <posts-table />
-    <v-btn to="/community/question/create" />
-  </div>
+  <v-container>
+    <v-row justify="end">
+      <v-btn to="/community/question/create">
+        Post 생성
+      </v-btn>
+    </v-row>
+    <posts-data-iterator />
+  </v-container>
 </template>
 
 <script>
-import PostsTable from '@/components/community/question/posts/PostsTable.vue';
+import PostsDataIterator from '@/components/community/question/posts/PostsDataIterator.vue';
 
 export default {
   name: 'QuestionPosts',
   components: {
-    PostsTable,
+    PostsDataIterator,
   },
 };
 </script>
