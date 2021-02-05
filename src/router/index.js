9473e71db5b7e6103d16a3751e72a1c5f40cdca5
Hyunmin Jung <jung@hyunmin.dev>
2021-02-05 18:57:55 +0900

    feat: change navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,18 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import routes from '@/router/routes';
-import Store from '@/store';
+import Routes from '@/router/routes';
 
 Vue.use(Router);
 
 const router = new Router({
   mode: 'history',
-  routes,
+  routes: Routes,
 });
 
 // eslint-disable-next-line no-unused-vars
 router.afterEach((to, from) => {
   document.title = `${to.meta.title} - Kalgory`;
-});
-
-router.beforeEach((to, from, next) => {
-  if (to.meta.requireAuth) {
-    if (Store.getters.getIsAuthLoading) {
-      next('/');
-    } else if (Store.getters.getIsAuth) {
-      next();
-    } else {
-      next('/');
-    }
-  } else {
-    next();
-  }
 });
 
 export default router;
