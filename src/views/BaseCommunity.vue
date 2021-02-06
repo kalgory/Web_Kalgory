09a2e9d0f6760cc78b988d4cb0f7ca08ffa280bb
Dongmin Kim <ehdals0211@naver.com>
2021-02-06 21:02:06 +0900

    feat: add toggle btn from toolbar

--- src/views/BaseCommunity.vue
+++ src/views/BaseCommunity.vue
@@ -1,11 +1,17 @@
 <template>
-  <router-view />
+  <div>
+    <community-toolbar />
+    <router-view />
+  </div>
 </template>
 
 <script>
+import CommunityToolbar from '@/views/community/CommunityToolbar.vue';
+
 export default {
   name: 'BaseCommunity',
   components: {
+    CommunityToolbar,
   },
 };
 </script>
---
+++ src/views/community/CommunityToolbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-toolbar class="elevation-0">
+    <v-spacer />
+    <v-btn
+      :disabled="this.$route.name.indexOf('information')!==-1"
+      to="/community/information"
+      exact
+    >
+      <v-icon>
+        mdi-information
+      </v-icon>
+    </v-btn>
+    <v-btn
+      :disabled="this.$route.name.indexOf('default')!==-1"
+      to="/community"
+      exact
+    >
+      <v-icon>
+        mdi-home
+      </v-icon>
+    </v-btn>
+    <v-btn
+      :disabled="this.$route.name.indexOf('question')!==-1"
+      to="/community/question"
+      exact
+    >
+      <v-icon>
+        mdi-frequently-asked-questions
+      </v-icon>
+    </v-btn>
+    <v-spacer />
+  </v-toolbar>
+</template>
+<script>
+export default {
+  name: 'CommunityToolbar',
+};
+</script>
