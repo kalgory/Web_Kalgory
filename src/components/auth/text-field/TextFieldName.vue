8dac54143ed3f037899212fa8080b67b76efcc0a
Hyunmin Jung <jung@hyunmin.dev>
2021-02-06 12:52:34 +0900

    feat: add TextFieildName.vue

---
+++ src/components/auth/text-field/TextFieldName.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-text-field
+    :value="value"
+    type="email"
+    label="Username"
+    prepend-inner-icon="mdi-account-outline"
+    placeholder="Type your username"
+    @input="onInput"
+  />
+</template>
+
+<script>
+export default {
+  name: 'TextFieldName',
+
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+
+  methods: {
+    onInput(value) {
+      this.$emit('input', value);
+    },
+  },
+};
+</script>
