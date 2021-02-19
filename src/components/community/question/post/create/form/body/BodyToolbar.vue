65cd9ed68dfd64eb0df94e6d1ea563e2c581a9b1
Dongmin Kim <ehdals0211@naver.com>
2021-02-19 15:50:17 +0900

    feat: create body text tool btn group

---
+++ src/components/community/question/post/create/form/body/BodyToolbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-toolbar
+    outlined
+    :elevation="0"
+  >
+    <v-btn-toggle
+      v-model="toggle_exclusive"
+      mandatory
+    >
+      <v-btn>
+        <v-icon>mdi-format-bold</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-format-italic</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-format-underline</v-icon>
+      </v-btn>
+    </v-btn-toggle>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'BodyToolbar',
+
+  data: () => ({
+    toggle_exclusive: undefined,
+  }),
+
+};
+</script>
+
+<style scoped>
+
+</style>
