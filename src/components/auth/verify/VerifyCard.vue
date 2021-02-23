4bde3f6e289f9a5f72f3ecb5cb4e1dd7d3e19a9c
Hyunmin Jung <jung@hyunmin.dev>
2021-02-23 20:55:43 +0900

    fix: fix appbar show bug

--- src/components/auth/verify/VerifyCard.vue
+++ src/components/auth/verify/VerifyCard.vue
@@ -6,12 +6,20 @@
     <auth-card-title :is-button-disabled="true">
       계속 하려면 이메일을 인증해주세요.
     </auth-card-title>
-    test
+
+    <v-card-actions>
+      <v-btn
+        @click="signOut"
+      >
+        sign out
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
 import AuthCardTitle from '@/components/auth/AuthCardTitle.vue';
+import { signOut } from '@/plugins/firebase/auth';
 
 export default {
   name: 'VerifyCard',
@@ -23,5 +31,27 @@ export default {
   data: () => ({
     isLoading: false,
   }),
+
+  methods: {
+    signOut() {
+      this.$store.commit('setIsAuthLoading', true);
+      signOut()
+        .then(() => {
+          this.$toasted.show('로그아웃 완료', {
+            type: 'success',
+            icon: 'mdi-account-outline',
+          });
+        })
+        .catch((error) => {
+          this.$toasted.show(error.message, {
+            type: 'error',
+            icon: 'mdi-account-outline',
+          });
+        })
+        .finally(() => {
+          this.$store.commit('setIsAuthLoading', false);
+        });
+    },
+  },
 };
 </script>
--- src/views/auth/AuthVerify.vue
+++ src/views/auth/AuthVerify.vue
@@ -1,5 +1,5 @@
 <template>
-  <verify-card />
+  <verify-card v-if="!isLoading" />
 </template>
 
 <script>
@@ -7,8 +7,53 @@ import VerifyCard from '@/components/auth/verify/VerifyCard.vue';
 
 export default {
   name: 'AuthVerify',
+
   components: {
     VerifyCard,
+  },
+
+  data: () => ({
+    isLoading: true,
+  }),
+
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.getIsAuthenticated;
+    },
+    isVerified() {
+      return this.$store.getters.getIsVerified;
+    },
+  },
+
+  watch: {
+    isAuthenticated(value) {
+      if (!value) {
+        this.$router.back();
+      }
+    },
+    isVerified(value) {
+      if (value) {
+        this.$toasted.global.success({ message: '인증을 마쳤습니다.' });
+        this.$router.back();
+      }
+    },
+  },
+
+  created() {
+    if (this.$store.getters.getIsAuthLoading) {
+      if (localStorage.getItem('isAuthenticated') === 'false') {
+        this.$router.back();
+      } else {
+        this.isLoading = false;
+      }
+    } else if (!this.isAuthenticated) {
+      this.$router.back();
+    } else {
+      if (this.isVerified) {
+        this.$router.back();
+      }
+      this.isLoading = false;
+    }
   },
 };
 </script>
