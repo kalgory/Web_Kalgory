8eac586c5119254cc0b754c5f873b87b9d604224
Dongmin Kim <ehdals0211@naver.com>
2021-03-01 20:04:01 +0900

    revert: revert baseconcept.vue

--- src/views/BaseConcept.vue
+++ src/views/BaseConcept.vue
@@ -1,52 +1,15 @@
 <template>
-  <v-container class="pa-4 text-center">
-    <v-row
-      class="fill-height"
-      align="center"
-      justify="center"
-    >
-      <v-hover v-slot="{ hover }">
-        <v-card
-          :elevation="hover ? 12 : 2"
-        >
-          <v-img
-            :src="item.img"
-            height="225px"
-          >
-            <v-card-title class="title white--text">
-              <v-row
-                class="fill-height flex-column"
-                justify="space-between"
-              >
-                <p class="mt-4 subheading text-left">
-                  {{ item.title }}
-                </p>
-              </v-row>
-            </v-card-title>
-          </v-img>
-        </v-card>
-      </v-hover>
-    </v-row>
+  <v-container>
+    concept
   </v-container>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    icons: ['mdi-rewind', 'mdi-play', 'mdi-fast-forward'],
-    item: [
-      {
-        title: 'New Releases',
-        text: 'It\'s New Release Friday',
-        subtext: 'Newly released songs. Updated daily.',
-        img: 'http://lorempixel.com/output/nightlife-q-c-640-480-5.jpg',
-      },
-
-    ],
-    transparent: 'rgba(255, 255, 255, 0)',
-  }),
+  name: 'BaseConcept',
 };
 </script>
 
 <style scoped>
+
 </style>
