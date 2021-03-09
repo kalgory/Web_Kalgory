d119900f5280abd6328416251448dec7a0eae00a
hyunmindev <jung@hyunmin.dev>
2021-03-09 17:55:05 +0900

    feat: add data iterator

--- src/views/problem/ProblemDefault.vue
+++ src/views/problem/ProblemDefault.vue
@@ -1,8 +1,20 @@
 <template>
-  <div />
+  <v-container>
+    <v-row>
+      <v-col>
+        <default-data-iterator />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
+import DefaultDataIterator from '@/components/problem/default/DefaultDataIterator.vue';
+
 export default {
   name: 'BaseProblem',
+
+  components: {
+    DefaultDataIterator,
+  },
 };
 </script>
